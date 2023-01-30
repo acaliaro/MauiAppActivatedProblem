0b8a31dddd00e96b4e0807a45bf7026066e6a013
Alessandro Caliaro <alessandro.caliaro@soluzione1.it>
2023-01-30 16:18:31 +0100

    Aggiungere i file di progetto.

---
+++ App.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiAppActivatedProblem;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        System.Diagnostics.Debug.WriteLine("CreateWindow", "CreateWindow");
+
+        Window window = base.CreateWindow(activationState);
+
+        window.Created += (s, e) =>
+        {
+            System.Diagnostics.Debug.WriteLine("Created", "CreateWindow");
+        };
+
+        window.Activated += (s, e) =>
+        {
+            System.Diagnostics.Debug.WriteLine("Activated", "CreateWindow");
+        };
+
+        window.Deactivated += (s, e) =>
+        {
+            System.Diagnostics.Debug.WriteLine("Deactivated", "CreateWindow");
+        };
+
+        return window;
+    }
+
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppActivatedProblem;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
